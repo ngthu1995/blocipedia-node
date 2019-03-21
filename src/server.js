6d6052af9411d0f2e82eb4e8efb678cb1fe79ebd
Thu Nguyen <ngthu1995@gmail.com>
2019-03-20 23:34:21 -0400

    modify server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
 const app = require("./app");
 const http = require("http");
+
+const port = normalizePort(process.env.PORT || "4000");
+app.set("port", port);
 const server = http.createServer(app);
 
-server.listen(4000);
+server.listen(port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
 server.on("listening", () => {
-  console.log("server is listening for request on port 4000");
+  console.log(
+    `server is listening for requests on port ${server.address().port}`
+  );
 });
